f109702d01e310c680c522160518bc950e5f51ac
Jeanpierre Chipa <chipa.robladillo@gmail.com>
2025-06-28 09:00:46 -0500

    Final

--- src/main/java/com/clinica/sistema/SistemaApplication.java
+++ src/main/java/com/clinica/sistema/SistemaApplication.java
@@ -1,4 +1,4 @@
-	package com.clinica.sistema;
+package com.clinica.sistema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
