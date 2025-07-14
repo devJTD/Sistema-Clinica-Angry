baf22148d31f270effe5dd5c0e717816bf9a8cae
Jeanpierre Chipa <chipa.robladillo@gmail.com>
2025-07-13 20:47:16 -0500

    Implementación Tomcat

--- src/main/java/com/clinica/sistema/SistemaApplication.java
+++ src/main/java/com/clinica/sistema/SistemaApplication.java
@@ -2,12 +2,18 @@ package com.clinica.sistema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SistemaApplication {
+public class SistemaApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SistemaApplication.class);
+    }
 }
